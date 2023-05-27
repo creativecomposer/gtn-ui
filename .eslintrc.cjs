83fc06fd97bbcc86e509606e5cf6c030c6294049
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-27 16:55:34 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: ["react-refresh", "react", "jsx-a11y", "@typescript-eslint"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Guess the Number</h1>
+    </header>
+  );
+}
+
+export default Header;
