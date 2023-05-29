4a726cc6ba523c03bc652ebce480e53cdd7dae5e
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 14:16:19 +0200

    Redirect link to home in the 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
     <article>
       <h3>Oops, that is a wrong route :-(</h3>
       <h3>Looks like you lost your way?</h3>
-      <button type="button">Go Home</button>
+      <Link className="button" to="/">
+        Go Home
+      </Link>
     </article>
   );
 }
