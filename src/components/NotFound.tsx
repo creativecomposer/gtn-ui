8d8d930d9f01f04bbfdfef51f00de7b47a2cb15a
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 09:02:55 +0200

    Add 404 page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+function NotFound() {
+  return (
+    <article>
+      <h3>Oops, that is a wrong route :-(</h3>
+      <h3>Looks like you lost your way?</h3>
+      <button type="button">Go Home</button>
+    </article>
+  );
+}
+
+export default NotFound;
