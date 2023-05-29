d1a0a1f17315b8b1ae847546105f6c1e1b43fda6
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 09:53:05 +0200

    Add user name display and logout button to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./Header.module.css";
+import { logout, selectCurrentUser } from "../state/authSlice";
+import { User } from "../types";
+import { AppDispatch } from "../state/store";
+
 function Header() {
+  const user: User = useSelector(selectCurrentUser);
+  const dispatch: AppDispatch = useDispatch();
+
+  const loggedInContent = function () {
+    return (
+      <>
+        <h2>{user.name}</h2>
+        <button type="button" onClick={() => dispatch(logout())}>
+          Logout
+        </button>
+      </>
+    );
+  };
+
   return (
-    <header>
-      <h1>Guess the Number</h1>
+    <header className={styles.header}>
+      <div>
+        <h1>Guess the Number</h1>
+        <h2>A simple game to improve math skills</h2>
+      </div>
+      <div>{user !== null ? loggedInContent() : <h2>Not logged in</h2>}</div>
     </header>
   );
 }
