accb90b86dab3983e16c27168444bb44e03a5327
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 09:53:40 +0200

    Hook up the router and redux store to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import GameContainer from "./components/GameContainer";
+import AuthContainer from "./components/AuthContainer";
+import AuthGuard from "./components/AuthGuard";
 
 function App() {
   return (
     <main>
       <Header />
+      <Routes>
+        <Route path="/" element={<AuthContainer />} />
+        <Route
+          path="/game"
+          element={
+            <AuthGuard>
+              <GameContainer />
+            </AuthGuard>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./picnic.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
