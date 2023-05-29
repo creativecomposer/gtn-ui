4c666d0f174bd5a59e245e4cd00f9d98011d5df2
Creative Composer <a.jesudhason@hellobetter.de>
2023-05-29 09:49:30 +0200

    Add GameStatus component to show the current status and number of guesses so far to the player

---
+++ src/components/GameStatus.tsx
@@ -0,0 +1,31 @@
+type PropsType = {
+  guessCount: number;
+  status: string;
+};
+
+function GameStatus({ guessCount, status }: PropsType) {
+  const getStatusText = function () {
+    let text = "";
+
+    if (status === "init") {
+      text = "Game started";
+    } else if (status === "high") {
+      text = "Guess is higher than the target";
+    } else if (status === "low") {
+      text = "Guess is lower than the target";
+    } else if (status === "correct") {
+      text = "You did it! Great work!!";
+    } else {
+      text = status === "" ? "Uknown" : status;
+    }
+    return <p>{text}</p>;
+  };
+  return (
+    <div>
+      {getStatusText()}
+      <p>Number of guesses: {guessCount}</p>
+    </div>
+  );
+}
+
+export default GameStatus;
